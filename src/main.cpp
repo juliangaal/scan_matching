33e39780bede5b62ddaab442dfeca731967f3907
juliangaal <juliangaal@protonmail.com>
2022-03-29 14:01:03 +0200

    intial c++ commit: pcl preprocessing, viewer

---
+++ src/main.cpp
@@ -0,0 +1,65 @@
+//
+// Created by julian on 29.03.22.
+//
+
+#include "viewer.h"
+#include "icp_LM.h"
+
+#include <pcl/common/centroid.h>
+#include <pcl/filters/filter.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <fmt/printf.h>
+
+template <typename T>
+void preprocess(typename pcl::PointCloud<T>::Ptr &cloud, float scale = 1000.f)
+{
+    // remove NaNs
+    pcl::Indices indices;
+    pcl::removeNaNFromPointCloud(*cloud, *cloud, indices);
+
+    // demean pcl
+    Eigen::Vector4f centroid;
+    pcl::compute3DCentroid(*cloud, centroid);
+    pcl::demeanPointCloud(*cloud, centroid, *cloud);
+
+    // scale
+    for (auto& p: cloud->points)
+    {
+        p.x *= scale;
+        p.y *= scale;
+        p.z *= scale;
+    }
+}
+
+template <typename T>
+void file2pcd(typename pcl::PointCloud<T>::Ptr& cloud, std::string filename)
+{
+    if (pcl::io::loadPCDFile<T>(filename, *cloud) == -1)
+    {
+        fmt::print("Couldn't read file {}\n", filename);
+        throw std::runtime_error("PCL IO Error");
+    }
+}
+
+int main(void)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr model(new pcl::PointCloud<pcl::PointXYZ>());
+    file2pcd<pcl::PointXYZ>(model, "../data/bun000_UnStructured.pcd");
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr scene(new pcl::PointCloud<pcl::PointXYZ>());;
+    file2pcd<pcl::PointXYZ>(scene, "../data/bun045_UnStructured.pcd");
+
+    preprocess<pcl::PointXYZ>(model);
+    preprocess<pcl::PointXYZ>(scene);
+
+    // args: grid_size, dist_thresh, iterations, model, scene, viewer
+
+    Viewer viewer("PCL Viewer");
+    viewer.add_pointcloud("model cloud", model, 3.0, 0, 0, 255);
+    viewer.add_pointcloud("scene cloud", scene, 4.0, 255, 0, 0);
+    viewer.show_viewer();
+
+    return 0;
+}
---
+++ src/viewer.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/visualization/cloud_viewer.h>
+
+/**
+  * @file viewer.h
+  * @author julian 
+  * @date 1/31/22
+ */
+
+class Viewer
+{
+public:
+    explicit Viewer(const std::string &name);
+    
+    ~Viewer() = default;
+    
+    void add_pointcloud(std::string id, const pcl::PointCloud<pcl::PointXYZRGBA>::Ptr &cloud, double size);
+
+    void
+    add_pointcloud(std::string id, const pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud, double size, int r,
+                           int g,
+                           int b);
+
+    void set_point_size(std::string id, double size);
+    
+    void
+    add_normals(std::string id, const pcl::PointCloud<pcl::PointXYZRGBA>::Ptr &cloud,
+                const pcl::PointCloud<pcl::Normal>::Ptr &normals, int level, float scale);
+    
+    void show_viewer();
+    
+    void add_point(const std::string &id, Eigen::Vector4d &matrix, double size, int r, int g, int b);
+
+private:
+    pcl::visualization::PCLVisualizer viewer;
+};
