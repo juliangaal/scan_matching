8a557e9e8ffaaf50df3f275c590eaea95004c1e8
juliangaal <gjulian@uos.de>
2022-03-30 22:23:57 +0200

    add jacobian, euler->rot conversion utils

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,25 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/catch_approx.hpp>
 
+#include "utils.h"
+#include "jacobian.h"
+
 TEST_CASE("test", "tests")
 {
     REQUIRE(0 == 0);
+}
+
+TEST_CASE("Jacobian", "jacobian")
+{
+    State state = State::Zero();
+    state[2] = M_PI;
+    
+    auto [R, t] = state_2_rot_trans(state);
+    pcl::Normal n(0, 0, 1);
+    
+    Eigen::Vector3f point(1, 1, 1);
+    point = R * point;
+    std::cout << point << "\n";
+    std::cout << R << "\n";
+//    auto J = jacobian_plane(point, n);
 }
